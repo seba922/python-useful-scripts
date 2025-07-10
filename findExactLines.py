9601a5a9fe295deab315935828d5d5ddeff53a4f
Sebastian <sebastian.wojcik@contractors.roche.com>
2025-07-10 11:07:45 +0200

    code cleaning

--- findExactLines.py
+++ findExactLines.py
@@ -7,7 +7,6 @@ def findExactLines():
         with open(r"data.log", 'r', encoding='utf-8') as dataFile, \
             open(f"result.log", 'w', encoding='utf-8') as outputFile:
             for index, line in enumerate(dataFile):
-                done = False
                 if pattern.match(line):
                     outputFile.write(f'index {index} {line}')
 
